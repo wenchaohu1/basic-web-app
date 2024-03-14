1b741559c1a40864a6823f108bfffc163607d47f
Wenchao Hu <wenchaoh@andrew.cmu.edu>
2024-03-14 15:42:26 -0400

    name query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,6 +6,17 @@ export default function QueryProcessor(query: string): string {
       "writer in the English language and the world's pre-eminent dramatist."
     );
   }
+  if (query.toLowerCase().includes("andrew")) {
+    return (
+      "wenchaoh"
+    );
+  }
+
+  if (query.toLowerCase().includes("your name")) {
+    return (
+      "Your Name"
+    );
+  }
 
   return "";
 }
